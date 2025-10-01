dbb67f2f58abcecef2fc60549fd8ed9ffe5d51fa
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-02 06:38:19 +0700

    Create Đường đi của quân Tịnh.java

---
+++ Đường đi của quân Tịnh.java
@@ -0,0 +1,46 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.net.Inet4Address;
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static int[] dx = { -1,-1,1,1};
+    public static int[] dy = {-1,1,-1,1 };
+    public static int[][] a = new int[505][505];
+    public static int n, s, t, u, v, m;
+    public static int cnt;
+
+    public static void nhap() {
+        n = sc.nextInt(); s = sc.nextInt(); t = sc.nextInt();
+        //m = sc.nextInt();
+       //u = sc.nextInt(); v = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public static void dfs(int i, int j) {
+        cnt++;
+        a[i][j] = 1;
+        for (int k = 0; k < 4; k++) {
+            int i1 = i + dx[k];
+            int j1 = j + dy[k];
+            if (i1 >= 1 && j1 >= 1 && i1 <= n && j1 <= n && a[i1][j1] == 0) {
+                dfs(i1, j1);
+            }
+        }
+    }
+    public static long[][] F = new long[505][505];
+    public static void main(String[] args) {
+        nhap();
+        dfs(s,t);
+        System.out.print(cnt);
+    }
+}
